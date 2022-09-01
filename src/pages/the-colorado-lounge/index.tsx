70cb769444c058993b857cc35e2417ff684ea50d
LIU YUANCONG <liuyuancong@team-lab.com>
2022-09-01 20:08:05 +0900

    feat: 📽 colorado lounge api processing

--- src/pages/the-colorado-lounge/index.tsx
+++ src/pages/the-colorado-lounge/index.tsx
@@ -1,4 +1,8 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react';
+import { FilmCard } from '~/components/pages/the-colorado-lounge/filmCard';
+import { Film } from '~/types/Films';
+import { getFilms } from '~/utils/films';
 
 /**
  * The Colorado Lounge page
@@ -8,9 +12,34 @@ import type { NextPage } from 'next'
  * 
  */
 const TheColoradoLoungePage: NextPage = () => {
+
+  const [films, setFilms] = useState<Film[]>([]);
+  // const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetchFilms();
+  }, []);
+
+  const fetchFilms = async () => {
+    // await getFilms();
+    // setFilms(getFilms());
+    console.log("here", films);
+  }
+
+  // function topTenFilmsDisplay() {
+  //   return films.map((film: Film, index: number) => {
+  //     return (
+  //       <FilmCard key={index} film={film} />
+  //     )
+  //   });
+  // }
+
   return (
     <main>
-      <p>this is the colorado lounge page!</p>
+      <h1>Welcome to the Colorado Lounge!</h1>
+      <h2>My top 10 films</h2>
+      { films.length === 0 ? <p>Loading...</p> : <FilmCard film={films[0]} />}
+      {/* <FilmCard film={films[0]} /> */}
     </main>
   )
 }
---
+++ src/utils/films.ts
@@ -0,0 +1,109 @@
+/**
+ * @file 映画情報取得関連のUtils
+ */
+
+import axios from "axios";
+import * as FilmConstants from "~/constants/films";
+import { months } from "~/constants/date";
+import { Film } from "~/types/Films";
+
+
+/**
+ * top 10 映画情報を取得する
+ * @returns Promise<Film[]>
+ */
+export const getFilms = async () => {
+  const films: Film[] = [];
+  FilmConstants.topTenFilmIds.forEach(async (id) => {
+    const response = await axios.get(`${FilmConstants.TMDB_API_BASE_URL}${id}?api_key=7c1960f09f8990909fa5fffb0f89043e`)
+      .then((res) => {
+        return filterFilmInfo(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    films.push(response as Film);
+  })
+
+  return films;
+}
+
+/**
+ * 映画情報フィルター
+ * 
+ * @param film 
+ * @returns filtered film info
+ */
+const filterFilmInfo = (film: any) => {
+  return {
+    title: film['title'],
+    originalTitle: film['original_title'],
+    genres: getGenreString(film['genres']),
+    releaseDate: convertFilmReleaseDate(film['release_date']),
+    overview: film['overview'],
+    productionCountries: getProductionCountries(film['production_countries']),
+    posterPath: getPosterUrl(film['poster_path']),
+    imdbUrl: getImdbUrl(film['imdb_id'])
+  } as Film;
+}
+
+/**
+ * 映画のジャンルを文字列に変換する
+ * 
+ * @param genres 
+ * @returns genre string
+ */
+const getGenreString = (genres: { id: number, name: string }[] ) => {
+  const genreString = genres.map((genre) => {
+    return genre.name;
+  }).join(", ");
+  return genreString;
+}
+
+
+/**
+ * 映画の公開日を文字列に変換する
+ * 
+ * @param releaseDate 
+ * @returns release date string
+ */
+const convertFilmReleaseDate = (releaseDate: string) => {
+  const date = releaseDate.split("-");
+  const year = date[0];
+  const month = months[date[1] as keyof typeof months];
+  const day = date[2];
+  return `${day} ${month} ${year}`;
+}
+
+/**
+ * 映画の制作国を文字列に変換する
+ * 
+ * @param productionCountries 
+ * @returns production countries string
+ */
+const getProductionCountries = (productionCountries: { iso_3166_1: string, name: string }[]) => {
+  const countries = productionCountries.map((country) => {
+    return country.name;
+  }).join(", ");
+  return countries;
+}
+
+/**
+ * 映画ポスターのURLを文字列に変換する
+ * 
+ * @param posterPath
+ * @returns poster path string
+ */
+const getPosterUrl = (posterPath: string) => {
+  return `${FilmConstants.POSTER_PATH}${posterPath}`;
+}
+
+/**
+ * 映画のIMDBのURLを文字列に変換する
+ * 
+ * @param imdbId 
+ * @returns imdb url string
+ */
+const getImdbUrl = (imdbId: string) => {
+  return `${FilmConstants.IMDB_BASE_URL}${imdbId}`;
+}
