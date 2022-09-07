9af37cd7265e327c0ab7d069943afb9562b97c7b
YUAN-TSUNG <satoshi.liu@icloud.com>
2022-09-08 00:49:53 +0900

    feature: 🐣 mask image completed!

--- src/components/pages/top/topWelcome/index.tsx
+++ src/components/pages/top/topWelcome/index.tsx
@@ -10,9 +10,18 @@ export const TopWelcome: React.FC = () => {
 
   const handleCursorMove = (e: MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const mousePercentX = Math.round(e.clientX / windowWidth * 100);
+    const mousePercentY = Math.round(e.clientY / windowHeight * 100);
+
     const pattern = document.getElementsByClassName('pattern')[0];
-    console.log(e);
-    pattern.setAttribute('style', `width: ${e.clientX}px; height: ${e.clientY}px;`);
+    pattern.setAttribute('style', `
+      display: block;
+      -webkit-mask-image: radial-gradient(circle at ${mousePercentX}% ${mousePercentY}%, black 20%, transparent 40%);
+    `);
   }
 
   return (
