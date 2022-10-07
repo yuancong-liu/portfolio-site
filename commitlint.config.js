e5cd1960208121b798329449ef239ecb5df8512c
LIU YUANCONG <liuyuancong@team-lab.com>
2022-10-07 19:34:07 +0900

    fix: 🍟 emoji added

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,6 @@
 // ref https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    '@commitlint/config-conventional'
+  ],
 };
