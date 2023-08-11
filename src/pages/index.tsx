3eb88421972f2bcb3d1c8974c62686c66fdf5466
YUAN-TSUNG <satoshi.liu@icloud.com>
2023-08-11 16:16:53 +0900

    🐛 unused vars removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from './index.module.scss';
 import { LayoutTop } from '~/components/layouts/top';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
