60063a06a4c063e11f60df0dcfea861c9b7ac159
YUAN-TSUNG <satoshi.liu@icloud.com>
2023-08-11 16:10:38 +0900

    🎉 renewal started!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,13 @@
 import '~/styles/globals.css'
-import fontsLoader from "~/interfaces/fontsLoader";
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
 import { appWithTranslation } from 'next-i18next';
 import { AnimatePresence } from 'framer-motion';
-import 'highlight.js/styles/default.css'
+import 'highlight.js/styles/default.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  useEffect(() => {
-    fontsLoader(document)
-  }, [])
-
   return (
-    <AnimatePresence exitBeforeEnter>
+    <AnimatePresence>
       <Component {...pageProps} />
     </AnimatePresence>
   );
