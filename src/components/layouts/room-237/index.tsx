6555c2634429728fb79f52a8b46a57d03eb1a0b6
YUAN-TSUNG <satoshi.liu@icloud.com>
2023-08-13 23:49:16 +0900

    🐛 old codes removed

--- src/components/layouts/room-237/index.tsx
+++ src/components/layouts/room-237/index.tsx
@@ -1,22 +1,7 @@
-import { useTranslation } from "next-i18next";
-import { useState } from "react";
-import { Language } from "~/components/common/languages/multilingual";
-
 export type Props = {
   children?: React.ReactNode;
 }
 
-export const LayoutRoom237: React.FC<Props> = ({ children }) => {
-  const { i18n } = useTranslation();
-  const [language, setLanguage] = useState(i18n.language || 'en')
-
-  return (
-    <div className={language}>
-      <Language
-        currentLanguage={language}
-        setLanguage={setLanguage}
-      />
-      {children}
-    </div>
-  );
+export const LayoutRoom237 = ({ children }: Props) => {
+  return children;
 }
