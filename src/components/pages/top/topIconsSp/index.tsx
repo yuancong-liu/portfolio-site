171e0fd8b0d8e141c090a5e67611ced66313fa3c
YUAN-TSUNG <satoshi.liu@icloud.com>
2023-08-14 00:00:24 +0900

    🚨 deploy test

--- src/components/pages/top/topIconsSp/index.tsx
+++ src/components/pages/top/topIconsSp/index.tsx
@@ -2,7 +2,7 @@ import LYMark from '~/assets/icons/ly-mark-sp.svg';
 import CMark from '~/assets/icons/c-mark-sp.svg';
 import styles from './index.module.scss';
 
-export const TopIconSp = () => {
+export const TopIconsSp = () => {
   return (
       <div className={styles['overall-wrapper']}>
         <div className={styles['icons-wrapper']}>
