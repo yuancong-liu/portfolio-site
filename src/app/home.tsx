b41e1673657c8ef2652f44d8d3ad407cb782b9df
YUAN-TSUNG <satoshi.liu@icloud.com>
2023-11-13 00:15:51 +0900

    ✨ dynamic import used to avoid the usage of window

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,13 +1,25 @@
 'use client';
+import dynamic from 'next/dynamic';
 // import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { NavBar } from '~/components/common/NavBar';
-import { IconsPc } from '~/components/pages/top/IconsPc';
-import { IconsSp } from '~/components/pages/top/IconsSp';
 import { useDeviceDetect } from '~/hooks';
+
+const IconsPc = dynamic(
+  () => import('~/components/pages/top/IconsPc').then((mod) => mod.IconsPc),
+  {
+    ssr: false,
+  },
+);
+
+const IconsSp = dynamic(
+  () => import('~/components/pages/top/IconsSp').then((mod) => mod.IconsSp),
+  {
+    ssr: false,
+  },
+);
 
 export const Home = () => {
   const { isPc } = useDeviceDetect();
-  if (window === undefined) return null;
 
   return (
     <>
--- src/hooks/libs/useDeviceDetect.ts
+++ src/hooks/libs/useDeviceDetect.ts
@@ -1,3 +1,4 @@
+'use client';
 import { useMediaQuery } from 'react-responsive';
 
 const BREAKPOINT = 768;
@@ -6,6 +7,8 @@ export const useDeviceDetect = () => {
   const isPc = useMediaQuery({
     query: `(min-width: ${BREAKPOINT}px)`,
   });
+
+  if (typeof window === 'undefined') return { isPc: undefined };
 
   return { isPc };
 };
