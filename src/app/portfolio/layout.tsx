fe0a2212a4ac6a864aa13a33cb48af0a63cf56ff
YUANCONG LIU <liuyuancong@team-lab.com>
2023-12-27 17:03:45 +0900

    ⚙️ add navigation to portfolio page anyway

---
+++ src/app/portfolio/layout.tsx
@@ -0,0 +1,16 @@
+import { NavBarCommon } from '~/components/common/navBarCommon';
+
+import styles from './layout.module.scss';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function BlogLayout({ children }: Props) {
+  return (
+    <div className={styles['page-wrapper']}>
+      <NavBarCommon />
+      {children}
+    </div>
+  );
+}
