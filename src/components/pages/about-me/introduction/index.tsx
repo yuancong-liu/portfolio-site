4c3f3e9da60f50fd54f8829180f005bc8b24cf09
YUAN-TSUNG <satoshi.liu@icloud.com>
2024-02-13 00:51:25 +0900

    ✨ name sparkle made

--- src/components/pages/about-me/introduction/index.tsx
+++ src/components/pages/about-me/introduction/index.tsx
@@ -1,14 +1,105 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 
 import classNames from 'classnames';
 import { motion } from 'framer-motion';
 
 import styles from './index.module.scss';
 
+export const Introduction = () => {
+  return (
+    <div className={styles['introduction-wrapper']}>
+      <h2 className={classNames(styles['hi'], styles['serif'])}>Hi!</h2>
+      <p className={classNames(styles['greeting'], styles['para'])}>
+        I am <NameSparkle />
+      </p>
+      <p className={classNames(styles['work'], styles['para'])}>
+        Currently a front-end engineer at{' '}
+        <span className={styles['serif']}>teamLab Inc.</span>
+      </p>
+      <p className={classNames(styles['hobbies'], styles['para'])}>
+        I like <HobbySlider />
+      </p>
+    </div>
+  );
+};
+
+/**
+ * NAME
+ */
+type Name = {
+  lang: string;
+  name: string;
+};
+
+const NAME = [
+  { lang: 'en', name: 'PAUL\nLIU' },
+  { lang: 'cn', name: '劉元聰' },
+  { lang: 'jp', name: 'リュウ\nゲンソウ' },
+  { lang: 'kr', name: '유원총' },
+] as Name[];
+
+const NameSparkle = memo(() => {
+  const [showNames, setShowNames] = useState<boolean>(false);
+
+  const nameVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: [0, 0.5, 0.5],
+      transition: {
+        duration: 1,
+        ease: 'easeInOut',
+      },
+    },
+  };
+
+  const handleTap = () => {
+    if (!showNames) {
+      setShowNames(true);
+      setTimeout(() => {
+        setShowNames(false);
+      }, 2000);
+    }
+  };
+
+  return (
+    <motion.span
+      className={classNames(
+        styles['serif'],
+        styles['name-sparkle'],
+        showNames && styles['-show'],
+      )}
+      onTap={handleTap}
+      onMouseEnter={() => setShowNames(true)}
+      onMouseLeave={() => setShowNames(false)}
+    >
+      PAUL Yuancong LIU.
+      {NAME.map((name) => (
+        <motion.span
+          variants={nameVariants}
+          initial="hidden"
+          animate={showNames ? 'visible' : 'hidden'}
+          onAnimationComplete={() => setShowNames((prev) => !prev)}
+          key={name.lang}
+          className={classNames(styles['entity'], styles[name.lang])}
+        >
+          {name.name}
+        </motion.span>
+      ))}
+    </motion.span>
+  );
+});
+
+NameSparkle.displayName = 'NameSparkle';
+
+/**
+ * HOBBIES
+ */
 const HOBBIES = ['CSS', 'Design', 'Horror Movies'];
 
-export const Introduction = () => {
+const HobbySlider = memo(() => {
   const [hobbies, setHobbies] = useState<string[]>(HOBBIES);
 
   useEffect(() => {
@@ -18,28 +109,14 @@ export const Introduction = () => {
 
     return () => clearInterval(intervalId);
   }, []);
-
   return (
-    <div className={styles['introduction-wrapper']}>
-      <h2 className={classNames(styles['hi'], styles['serif'])}>Hi!</h2>
-      <p className={classNames(styles['greeting'], styles['para'])}>
-        I am <span className={styles['serif']}>PAUL YUANCONG LIU.</span>
-      </p>
-      <p className={classNames(styles['work'], styles['para'])}>
-        Currently a front-end engineer at{' '}
-        <span className={styles['serif']}>teamLab Inc.</span>
-      </p>
-      <p className={classNames(styles['hobbies'], styles['para'])}>
-        I like{' '}
-        <span className={classNames(styles['serif'], styles['hobbies-slider'])}>
-          <motion.span
-            className={classNames(styles['entity'], styles['-prev'])}
-          >
-            {hobbies[0]}
-          </motion.span>
-          <motion.span className={styles['entity']}>{hobbies[1]}</motion.span>
-        </span>
-      </p>
-    </div>
+    <span className={classNames(styles['serif'], styles['hobbies-slider'])}>
+      <motion.span className={classNames(styles['entity'], styles['-prev'])}>
+        {hobbies[0]}
+      </motion.span>
+      <motion.span className={styles['entity']}>{hobbies[1]}</motion.span>
+    </span>
   );
-};
+});
+
+HobbySlider.displayName = 'HobbySlider';
