d67f60c6c3e023879d3e9f6c9242b2e190fc3d50
YUAN-TSUNG <satoshi.liu@icloud.com>
2024-02-25 19:18:33 +0900

    ✨ theme color set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,20 @@
 import { Analytics } from '@vercel/analytics/react';
+import { Viewport } from 'next';
 import '~/styles/globals.scss';
 
 type Props = {
   children: React.ReactNode;
 };
 
+export const viewport: Viewport = {
+  themeColor: '#132043',
+};
+
 const RootLayout = ({ children }: Props) => {
   return (
     <html lang="en">
       <body>
-        <div className="overall-background-grid" />
-        <div className="overall-background-gradient" />
+        <div className="overall-background" />
         {children}
       </body>
       <Analytics />
