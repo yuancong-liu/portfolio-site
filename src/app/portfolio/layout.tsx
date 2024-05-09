077563ac5ce16d56bf33281759f08ff64d02ad43
YUANCONG LIU <yuancong.liu@bit-realty.com>
2024-05-09 09:20:32 +0900

    😅 minor layout misarrangements fixed

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -3,12 +3,14 @@ import { ReactNode } from 'react';
 import { NavBar } from '~/components/common/navBar';
 import { ThemeRadio } from '~/components/common/themeRadio';
 
+import styles from './layout.module.scss';
+
 type Props = {
   children: ReactNode;
 };
 
 const AboutMeLayout = ({ children }: Props) => (
-  <div>
+  <div className={styles['page-wrapper']}>
     <NavBar />
     <ThemeRadio />
     {children}
